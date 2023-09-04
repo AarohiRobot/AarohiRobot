3e1f2b559a96509709ac1397520692c03f048d36
AarohiRobot <143418191+AarohiRobot@users.noreply.github.com>
2023-09-05 03:09:13 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,38 @@
-// script.js
-const joystick = document.getElementById("joystick");
-const stick = document.getElementById("stick");
-const angleDisplay = document.getElementById("angle");
+<script>
+    const joystick = document.getElementById("joystick");
+    const stick = document.getElementById("stick");
+    const angleDisplay = document.getElementById("angle");
 
-let isDragging = false; 
+    let isDragging = false;
 
-joystick.addEventListener("mousedown", (e) => {
-    isDragging = true;
-    joystick.style.transition = "none";
-    stick.style.transition = "none";
-    moveStick(e);
-});
-
-document.addEventListener("mousemove", (e) => {
-    if (isDragging) {
+    joystick.addEventListener("mousedown", (e) => {
+        isDragging = true;
+        joystick.style.transition = "none";
+        stick.style.transition = "none";
         moveStick(e);
-    }
-});
+    });
 
-document.addEventListener("mouseup", () => {
-    isDragging = false;
-    joystick.style.transition = "all 0.2s ease";
-    stick.style.transition = "all 0.2s ease";
-    stick.style.transform = "translate(-50%, -50%)";
-});
+    document.addEventListener("mousemove", (e) => {
+        if (isDragging) {
+            moveStick(e);
+        }
+    });
 
-function moveStick(e) {
-    const rect = joystick.getBoundingClientRect();
-    const centerX = rect.left + rect.width / 2;
-    const centerY = rect.top + rect.height / 2;
-    const deltaX = e.clientX - centerX;
-    const deltaY = e.clientY - centerY;
-    const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2);  
-    const maxDistance = rect.width / 2 - stick.clientWidth / 2;
+    document.addEventListener("mouseup", () => {
+        isDragging = false;
+        joystick.style.transition = "all 0.2s ease";
+        stick.style.transition = "all 0.2s ease";
+        stick.style.transform = "translate(-50%, -50%)";
+    });
 
-    if (distance <= maxDistance) {
+    function moveStick(e) {
+        const rect = joystick.getBoundingClientRect();
+        const centerX = rect.left + rect.width / 2;
+        const centerY = rect.top + rect.height / 2;
+        const deltaX = e.clientX - centerX;
+        const deltaY = e.clientY - centerY;
+        const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
         stick.style.transform = `translate(-50%, -50%) translate(${deltaX}px, ${deltaY}px)`;
-    } else {
-        const angle = Math.atan2(deltaY, deltaX);
-        const newX = Math.cos(angle) * maxDistance;
-        const newY = Math.sin(angle) * maxDistance;
-        stick.style.transform = `translate(-50%, -50%) translate(${newX}px, ${newY}px)`;
+        angleDisplay.textContent = `Angle: ${angle.toFixed(2)}°`;
     }
-
-    const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
-    angleDisplay.textContent = `Angle: ${angle.toFixed(2)}°`;
-}
+</script>
