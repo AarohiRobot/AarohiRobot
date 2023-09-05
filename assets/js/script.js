562e3d3812d99eb3f85116efc85f4433583f6c80
AarohiRobot <143418191+AarohiRobot@users.noreply.github.com>
2023-09-05 13:53:44 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,58 @@
-/* styles.css */
-body {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    margin: 0;
-}
+// script.js
+const joystick = document.getElementById("joystick");
+const stick = document.getElementById("stick");
+const angleDisplay = document.getElementById("angle");
 
-.joystick-container {
-    position: relative;
-}
+let isDragging = false;
 
-.joystick {
-    width: 150px;
-    height: 150px;
-    background-color: #3498db;
-    border-radius: 50%;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
+joystick.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    joystick.style.transition = "none";
+    stick.style.transition = "none";
+    moveStick(e);
+});
 
-.stick {
-    width: 50px;
-    height: 50px;
-    background-color: #e74c3c;
-    border-radius: 50%;
-    position: absolute;
-    cursor: pointer;
+document.addEventListener("mousemove", (e) => {
+    if (isDragging) {
+        moveStick(e);
+    }
+});
+
+document.addEventListener("mouseup", () => {
+    isDragging = false;
+    joystick.style.transition = "all 0.2s ease";
+    stick.style.transition = "all 0.2s ease";
+    stick.style.transform = "translate(-50%, -50%)";
+    // Here, you can send a stop command to your robot (set all wheel speeds to 0).
+});
+
+function moveStick(e) {
+    const rect = joystick.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    const deltaX = e.clientX - centerX;
+    const deltaY = e.clientY - centerY;
+    const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+
+    // Calculate linear velocity (Vx) and angular velocity (Omega) based on joystick position
+    // You'll need to map joystick position to robot velocities here
+
+    // Example: Map joystick position to linear velocity and angular velocity
+    const maxJoystickDistance = rect.width / 2;
+    const maxLinearVelocity = 1.0; // Adjust as needed
+    const maxAngularVelocity = 1.0; // Adjust as needed
+
+    // Scale the joystick position to match your robot's requirements
+    const scaledDeltaX = (deltaX / maxJoystickDistance);
+    const scaledDeltaY = (deltaY / maxJoystickDistance);
+    
+    // Apply scaling to Vx and Omega
+    const Vx = scaledDeltaY * maxLinearVelocity;
+    const Omega = scaledDeltaX * maxAngularVelocity;
+
+    // Display the calculated angle
+    angleDisplay.textContent = `Angle: ${angle.toFixed(2)}°`;
+
+    // Here, you can send the calculated Vx and Omega to control your robot.
+    // You may need to communicate with your robot's hardware or software to send commands.
 }
