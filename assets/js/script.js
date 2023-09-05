244a2ca5f05ecad0bb4f3c17f8e1be7d4de57631
AarohiRobot <143418191+AarohiRobot@users.noreply.github.com>
2023-09-05 13:33:49 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,7 @@ document.addEventListener("mouseup", () => {
     joystick.style.transition = "all 0.2s ease";
     stick.style.transition = "all 0.2s ease";
     stick.style.transform = "translate(-50%, -50%)";
+    // Here, you can send a stop command to your robot (set all wheel speeds to 0).
 });
 
 function moveStick(e) {
@@ -32,6 +33,20 @@ function moveStick(e) {
     const deltaX = e.clientX - centerX;
     const deltaY = e.clientY - centerY;
     const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
-    stick.style.transform = `translate(-50%, -50%) translate(${deltaX}px, ${deltaY}px)`;
+
+    // Calculate linear velocity (Vx) and angular velocity (Omega) based on joystick position
+    // You'll need to map joystick position to robot velocities here
+
+    // Example: Map joystick position to linear velocity and angular velocity
+    const maxJoystickDistance = rect.width / 2;
+    const maxLinearVelocity = 1.0; // Adjust as needed
+    const maxAngularVelocity = 1.0; // Adjust as needed
+    const Vx = (deltaY / maxJoystickDistance) * maxLinearVelocity;
+    const Omega = (deltaX / maxJoystickDistance) * maxAngularVelocity;
+
+    // Display the calculated angle
     angleDisplay.textContent = `Angle: ${angle.toFixed(2)}°`;
+
+    // Here, you can send the calculated Vx and Omega to control your robot.
+    // You may need to communicate with your robot's hardware or software to send commands.
 }
