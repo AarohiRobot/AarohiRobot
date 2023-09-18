9bb5cdade91323eb76b56257c9a58ecf383c08a5
AarohiRobot <143418191+AarohiRobot@users.noreply.github.com>
2023-09-19 00:54:39 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
-// JavaScript to capture button clicks and display output
+// JavaScript to capture button clicks and dynamic highlighting
 const buttons = document.querySelectorAll('.button');
 const output = document.getElementById('output');
+let selectedButton = null;
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const direction = button.id;
         output.textContent = `You clicked ${direction}`;
+        
+        if (selectedButton) {
+            selectedButton.style.backgroundColor = "#fff"; // Reset previously selected button
+        }
+        
+        selectedButton = button;
+        selectedButton.style.backgroundColor = "#ff9900"; // Highlight the clicked button
     });
 });
