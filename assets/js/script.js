3c7c137fbf7306bbc20787ca9ef533c95aaed5ff
AarohiRobot <143418191+AarohiRobot@users.noreply.github.com>
2023-09-19 00:38:42 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-   // JavaScript to capture button clicks and display output
+// JavaScript to capture button clicks and display output
 const buttons = document.querySelectorAll('.button');
 const output = document.getElementById('output');
 
