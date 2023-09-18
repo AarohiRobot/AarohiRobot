2509421f8446ef172687a1fb04f984991f2f76c1
AarohiRobot <143418191+AarohiRobot@users.noreply.github.com>
2023-09-19 00:32:06 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
-// JavaScript to capture button clicks
+// JavaScript to capture button clicks and display output
 const buttons = document.querySelectorAll('.button');
+const output = document.getElementById('output');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const direction = button.id;
-        alert(`You clicked ${direction}`);
+        output.textContent = `You clicked ${direction}`;
     });
 });
