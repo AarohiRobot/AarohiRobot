d9a6aa14f79d89efa91c05079a1e2af89bb4eacf
AarohiRobot <143418191+AarohiRobot@users.noreply.github.com>
2023-09-21 01:00:21 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,53 @@
+const mqtt = require('mqtt');
+
 // JavaScript to capture button clicks and publish messages to HiveMQ Cloud MQTT broker
 const buttons = document.querySelectorAll('.button');
 const output = document.getElementById('output');
 let selectedButton = null;
 
-// Define your HiveMQ Cloud broker connection details
-const mqttBroker = "62aa3c9521834e62b0fbdd628046ee46.s1.eu.hivemq.cloudt"; // Replace with your HiveMQ Cloud instance URL and port
-const mqttClient = new Paho.MQTT.Client(mqttBroker, "web_" + parseInt(Math.random() * 100, 10));
+// Define your HiveMQ Cloud MQTT credentials
+const mqttUsername = "aarohi"; // Replace with your HiveMQ Cloud username
+const mqttPassword = "Aarohi@pro1"; // Replace with your HiveMQ Cloud password
+const clientId = "web_" + parseInt(Math.random() * 100, 10);
 
-mqttClient.onConnectionLost = function (responseObject) {
-    if (responseObject.errorCode !== 0) {
-        console.log("Connection lost: " + responseObject.errorMessage);
-    }
-};
+// Define the MQTT broker URL and port (for HiveMQ Cloud)
+const brokerUrl = "62aa3c9521834e62b0fbdd628046ee46.s1.eu.hivemq.cloud";
 
-mqttClient.connect({
-    onSuccess: onConnect,
-    useSSL: true, // Set to true for secure communication
-    userName: "aarohi", // Replace with your HiveMQ Cloud username
-    password: "Aarohi@pro1", // Replace with your HiveMQ Cloud password
+// Create an MQTT client instance
+const mqttClient = mqtt.connect(brokerUrl, {
+  username: aarohi,
+  password: Aarohi@pro1,
+  clientId: 62aa3c9521834e62b0fbdd628046ee46,
+  clean: true // Set to true to start with a clean session
 });
 
-function onConnect() {
-    console.log("Connected to HiveMQ Cloud MQTT broker");
+mqttClient.on('connect', () => {
+  console.log('Connected to HiveMQ Cloud MQTT broker');
+});
 
-    // Subscribe to MQTT topics (if needed)
-    // mqttClient.subscribe("your_topic");
-}
+mqttClient.on('error', (error) => {
+  console.error('MQTT error:', error);
+});
 
+// Function to send commands
 function sendCommand(command) {
-    // Publish the command to a specific MQTT topic
-    const message = new Paho.MQTT.Message(command);
-    message.destinationName = "your_topic"; // Replace with the topic you want to publish to
-    mqttClient.send(message);
-    console.log("Sent command: " + command);
+  const direction = command.id;
+  output.textContent = `You clicked ${direction}`;
+
+  if (selectedButton) {
+    selectedButton.style.backgroundColor = "#fff"; // Reset previously selected button
+  }
+
+  selectedButton = command;
+  selectedButton.style.backgroundColor = "#ff9900"; // Highlight the clicked button
+
+  // Publish the command to an MQTT topic (e.g., "your-topic")
+  mqttClient.publish("your-topic", direction, { qos: 0 });
 }
 
+// Add click event listeners to buttons
 buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const direction = button.id;
-        output.textContent = `You clicked ${direction}`;
-
-        if (selectedButton) {
-            selectedButton.style.backgroundColor = "#fff"; // Reset previously selected button
-        }
-
-        selectedButton = button;
-        selectedButton.style.backgroundColor = "#ff9900"; // Highlight the clicked button
-
-        // Send the command via MQTT when a button is clicked
-        sendCommand(direction);
-    });
+  button.addEventListener('click', () => {
+    sendCommand(button);
+  });
 });
