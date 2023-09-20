cb02ba03c25f809d8b3307ba2e42e4e1ba160356
AarohiRobot <143418191+AarohiRobot@users.noreply.github.com>
2023-09-20 17:27:53 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,37 @@ const buttons = document.querySelectorAll('.button');
 const output = document.getElementById('output');
 let selectedButton = null;
 
+// Define your HiveMQ credentials
+const brokerUrl = "2736c350aa8d44eba0062e18cc424905.s2.eu.hivemq.cloud";
+const port = 8883;
+
+// MQTT client setup
+const mqttClient = new Paho.MQTT.Client(brokerUrl, port, "web_" + parseInt(Math.random() * 100, 10));
+
+mqttClient.onConnectionLost = function (responseObject) {
+    if (responseObject.errorCode !== 0) {
+        console.log("Connection lost: " + responseObject.errorMessage);
+    }
+};
+
+mqttClient.onMessageArrived = function (message) {
+    console.log("Message arrived: " + message.payloadString);
+    // Handle feedback from the robot here (if needed)
+};
+
+mqttClient.connect({ onSuccess: onConnect });
+
+function onConnect() {
+    console.log("Connected to MQTT broker");
+}
+
+function sendCommand(command) {
+    const message = new Paho.MQTT.Message(command);
+    message.destinationName = "robot/control"; // Publish to the control topic
+    mqttClient.send(message);
+    console.log("Sent command: " + command);
+}
+
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const direction = button.id;
@@ -14,5 +45,8 @@ buttons.forEach(button => {
         
         selectedButton = button;
         selectedButton.style.backgroundColor = "#ff9900"; // Highlight the clicked button
+
+        // Send the command via MQTT when a button is clicked
+        sendCommand(direction);
     });
 });
