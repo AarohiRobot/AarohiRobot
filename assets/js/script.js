2106c4327b05da17b64d425419644acbfd96128a
AarohiRobot <143418191+AarohiRobot@users.noreply.github.com>
2023-09-21 01:20:10 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,13 @@
-const mqtt = require('mqtt');
-
-// JavaScript to capture button clicks and publish messages to HiveMQ Cloud MQTT broker
+// JavaScript to capture button clicks and publish messages using MQTT.js
 const buttons = document.querySelectorAll('.button');
 const output = document.getElementById('output');
 let selectedButton = null;
-  
-// Define your HiveMQ Cloud MQTT credentials
-const mqttUsername = "aarohi"; // Replace with your HiveMQ Cloud username
-const mqttPassword = "Aarohi@pro1"; // Replace with your HiveMQ Cloud password
-const clientId = "web_" + parseInt(Math.random() * 100, 10);             
 
 // Define the MQTT broker URL and port (for HiveMQ Cloud)
 const brokerUrl = "62aa3c9521834e62b0fbdd628046ee46.s1.eu.hivemq.cloud";
 
 // Create an MQTT client instance
-const mqttClient = mqtt.connect(brokerUrl, {
-  username: "aarohi",
-  password: "Aarohi@pro1",
-  clientId: "62aa3c9521834e62b0fbdd628046ee46",
-  clean: true // Set to true to start with a clean session
-});
+const mqttClient = mqtt.connect(brokerUrl);
 
 mqttClient.on('connect', () => {
   console.log('Connected to HiveMQ Cloud MQTT broker');
