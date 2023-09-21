bf078d2bd391300dcd70933058510766e834641b
AarohiRobot <143418191+AarohiRobot@users.noreply.github.com>
2023-09-21 09:41:24 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-import mqtt from './assets/js/mqtt.js'; // Adjust the path to the correct location of mqtt.js
+/*import mqtt from './assets/js/mqtt.js'; // Adjust the path to the correct location of mqtt.js
 // JavaScript to capture button clicks and publish messages using MQTT.js
 
 const buttons = document.querySelectorAll('.button');
@@ -41,3 +41,33 @@ buttons.forEach(button => {
     sendCommand(button);
   });
 });
+*/
+var mqtt = require('mqtt')
+
+var options = {
+    host: '95a0778f498e4b0282b640b05341507c.s1.eu.hivemq.cloud',
+    port: 8883,
+    protocol: 'mqtts',
+    username: 'Aryan',
+    password: 'AryanMore123'
+}
+
+// initialize the MQTT client
+var client = mqtt.connect(options);
+
+// setup the callbacks
+client.on('connect', function () {
+    console.log('Connected');
+});
+
+client.on('error', function (error) {
+    console.log(error);
+});
+
+client.on('message', function (topic, message) {
+    // called each time a message is received
+    console.log('Received message:', topic, message.toString());
+});
+
+// publish message 'Hello' to topic 'my/test/topic'
+client.publish('encyclopedia/temperature', 'Hello');
