4fed6d9a4dd98ebe17a5092a4fb26c86bd50bf24
AarohiRobot <143418191+AarohiRobot@users.noreply.github.com>
2023-09-21 10:38:24 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 const buttons = document.querySelectorAll('.button');
 const output = document.getElementById('output');
 let selectedButton = null;
-
+     
 // Define the MQTT broker URL and port (for HiveMQ Cloud)
 const brokerUrl = "62aa3c9521834e62b0fbdd628046ee46.s1.eu.hivemq.cloud";
     
